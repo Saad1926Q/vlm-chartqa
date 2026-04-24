b35aa1e8b11953ef1456b99b1598f2762901926a
Saad1926Q <saad.ahmed1926q@gmail.com>
2026-04-24 22:31:31 +0530

    silly

--- src/vlm_chartqa/model.py
+++ src/vlm_chartqa/model.py
@@ -12,20 +12,21 @@ def load_model(lora_path=None):
         gpu_memory_utilization=0.8,  # Reduce if out of memory
     )
 
-    model = FastVisionModel.get_peft_model(
-        model,
-        finetune_vision_layers=False,
-        finetune_language_layers=True,
-        finetune_attention_modules=True,
-        finetune_mlp_modules=True,
-        r=LORA_RANK,  # The larger, the higher the accuracy, but might overfit
-        lora_alpha=16,  # Recommended alpha == r at least
-        lora_dropout=0,
-        bias="none",
-        random_state=3407,
-        use_rslora=False,
-        loftq_config=None,
-        use_gradient_checkpointing="unsloth",  # Reduces memory usage
-    )
+    if not lora_path:
+        model = FastVisionModel.get_peft_model(
+            model,
+            finetune_vision_layers=False,
+            finetune_language_layers=True,
+            finetune_attention_modules=True,
+            finetune_mlp_modules=True,
+            r=LORA_RANK,
+            lora_alpha=16,
+            lora_dropout=0,
+            bias="none",
+            random_state=3407,
+            use_rslora=False,
+            loftq_config=None,
+            use_gradient_checkpointing="unsloth",
+        )
 
     return model, tokenizer
